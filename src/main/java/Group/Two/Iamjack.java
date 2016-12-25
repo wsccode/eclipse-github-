f3c1bd95107fadc3de2fe16c2d51e175fb0be182
wsccode <shouchangwang@outlook.com>
2016-12-25 16:01:57 +0800

    Signed-off-by: wsccode <shouchangwang@outlook.com>
    这是第二个分支节点

---
+++ src/main/java/Group/Two/Iamjack.java
@@ -0,0 +1,11 @@
+package Group.Two;
+
+public class Iamjack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		App app = new App();
+		
+	}
+
+}
---
+++ src/main/java/Group/Two/SecondApp.java
@@ -0,0 +1,12 @@
+package Group.Two;
+
+public class SecondApp {
+	public SecondApp() {
+		// TODO Auto-generated constructor stub
+		System.out.println(this);
+	}
+	
+	public static void main(String[] args) {
+		new SecondApp();
+	}
+}
