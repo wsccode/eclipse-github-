b32867f973b7ac2c8261b4db7ed53293465a4452
wsccode <shouchangwang@outlook.com>
2016-12-25 15:24:28 +0800

    这是从eclipse中向github里面push项目

---
+++ src/main/java/Group/Two/App.java
@@ -0,0 +1,26 @@
+package Group.Two;
+
+
+import com.google.gson.Gson;
+
+
+class Person{
+	Person(){}
+	Person(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	String name;
+	int age;
+}
+
+public class App 
+{
+	private static Gson gson = new Gson();
+    public static void main( String[] args )
+    {
+    	Person person = new Person("John", 30);
+        String personToJson = gson.toJson(person);
+        System.out.println(personToJson);
+    }
+}
