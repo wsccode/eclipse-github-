06beea979e4f30ff329dc39c5a4ae36f2819ac07
wsccode <shouchangwang@outlook.com>
2016-12-25 15:47:57 +0800

    Signed-off-by: wsccode <shouchangwang@outlook.com>

--- src/main/java/Group/Two/AppTest.java
+++ src/main/java/Group/Two/AppTest.java
@@ -4,6 +4,7 @@ public class AppTest {
 	public AppTest() {
 		System.out.println(this);
 		// TODO Auto-generated constructor stub
+		System.out.println(null);
 	}
 	
 	public static void main(String[] args) {
